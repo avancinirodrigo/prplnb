488ce9208dcfe8b15c94a1a85b6bd4131f211651
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2023-03-05 20:46:04 +0000

    fixing render relative path problem

--- webapp/__init__.py
+++
@@ -1,8 +0,0 @@
-# from flask import Flask
-# from .config import Config
-# from .api import bp
-
-# def create_app(config=Config):
-#     app = Flask(__name__)
-#     app.register_blueprint(bp, url_prefix='/')
-#     return app
--- webapp/main.py
+++ webapp/main.py
@@ -1,3 +1,8 @@
+import os
+import sys
+path = os.path.join(os.path.dirname(__file__), os.pardir)
+sys.path.append(path)
 from .api import create_app
+
 
 app = create_app()
